995258b6daed8d4f1eae4aaae399c7b77c226001
sangsakaa <zainuri717@gmail.com>
2022-11-10 08:58:13 +0700

    ok fix

--- resources/views/admin/suratkeluar/editsuratkeluar.blade.php
+++ resources/views/admin/suratkeluar/editsuratkeluar.blade.php
@@ -15,18 +15,18 @@
             <div class=" grid sm:grid-cols-2 grid-cols-1  gap-2 capitalize">
                 <div class=" grid grid-cols-2 gap-2">
                     <label>Tanggal Surat Keluar</label>
-                    <div> <input type="date" name="tanggal_keluar" class=" w-full  border px-1 py-1 " title="Tanggal Surat Keluar" required value="{{$suratkeluar->tanggal_keluar}}"></div>
+                    <div> <input type="date" name="tanggal_keluar" class=" w-full  border text-sm px-1 py-1 " title="Tanggal Surat Keluar" required value="{{$suratkeluar->tanggal_keluar}}"></div>
                     <label for="">Nomor Surat</label>
-                    <div> <input type="text" name="nomor_surat" class=" w-full capitalize border px-1 py-1" placeholder="nomor surat" value="{{$suratkeluar->nomor_surat}}"></div>
+                    <div> <input type="text" name="nomor_surat" class=" w-full capitalize border text-sm px-1 py-1" placeholder="nomor surat" value="{{$suratkeluar->nomor_surat}}"></div>
                     <label for="">Tujuan</label>
-                    <input type="text" name="tujuan" class=" capitalize border px-1 py-1" placeholder="tujuan" value="{{$suratkeluar->tujuan}}">
+                    <input type="text" name="tujuan" class=" capitalize border text-sm px-1 py-1" placeholder="tujuan" value="{{$suratkeluar->tujuan}}">
                 </div>
                 <div class=" grid grid-cols-2 gap-2">
                     <label for="">uraian</label>
-                    <input type="text" name="uraian" class=" capitalize border px-1 py-1" placeholder="uraian" value="{{$suratkeluar->uraian}}">
+                    <input type="text" name="uraian" class=" capitalize border text-sm px-1 py-1" placeholder="uraian" value="{{$suratkeluar->uraian}}">
                     <label for="">perihal</label>
-                    <input type="text" name="perihal" class=" capitalize border px-1 py-1" placeholder="perihal" value="{{$suratkeluar->perihal}}">
-                    <input type="file" name="file" id="">
+                    <input type="text" name="perihal" class=" capitalize border text-sm px-1 py-1" placeholder="perihal" value="{{$suratkeluar->perihal}}">
+                    <input type="file" name="file" id="" class=" text-sm">
                     <button type=" submit" class=" bg-purple-600 text-white py-1 px-4"> Update Surat Keluar
                     </button>
                 </div>
